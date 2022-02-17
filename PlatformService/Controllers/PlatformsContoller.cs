6eb629bcd5ea5fb0d451672abce7f27d32612a72
wallaceleonel <wallace.leonell@outlook.com>
2022-02-17 17:04:12 -0300

    controller

--- PlatformService/Controllers/PlatformsContoller.cs
+++ PlatformService/Controllers/PlatformsContoller.cs
@@ -1,10 +1,13 @@
+using System;
+using System.Collections.Generic;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
+using PlatformService.Dtos;
 
 namespace PlatformService.Controllers
 {
-    [Route("api;[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class PlatformsController : ControllerBase
     {
@@ -16,5 +19,15 @@ namespace PlatformService.Controllers
             _repository = repository;
             _mapper = mapper;           
         }
-    }
+
+          [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms .... ");
+
+            var PlatformItem = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(PlatformItem));
+        }
+    }     
 }
