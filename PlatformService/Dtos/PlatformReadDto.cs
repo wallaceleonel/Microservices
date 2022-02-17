999b6076c58c1c991c5d19fb330cc202ddb36bf5
wallaceleonel <wallace.leonell@outlook.com>
2022-02-17 12:42:13 -0300

    read and create

--- PlatformService/Dtos/PlatformReadDto.cs
+++ PlatformService/Dtos/PlatformReadDto.cs
@@ -2,12 +2,9 @@ namespace PlatformService.Dtos
 {
     public class PlatformReadDto
     {   
-        public int Id { get; set; }
-        
+        public int Id { get; set; }        
         public string Name { get; set; }
-      
         public string Publisher { get; set; }
-      
         public string  Cost { get; set; }
     }
 }
