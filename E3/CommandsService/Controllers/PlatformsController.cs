7cbaa7a2b3d8c72fe71233711d63adf3b23a250d
wallaceleonel <wallace.leonell@outlook.com>
2022-02-22 18:28:57 -0300

    test ok

--- E3/CommandsService/Controllers/PlatformsController.cs
+++ E3/CommandsService/Controllers/PlatformsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
 {
-    [Route("api/c/[cpntroller]")]
+    [Route("api/c/[controller]")]
     [ApiController]
     public class PlatformsController : ControllerBase
     {
