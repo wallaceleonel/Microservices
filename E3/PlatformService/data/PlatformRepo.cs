4cb65a362b9243dbcf033f2bbe9523f513f9add4
wallaceleonel <wallace.leonell@outlook.com>
2022-02-25 19:39:17 -0300

    ..

--- E3/PlatformService/data/PlatformRepo.cs
+++ E3/PlatformService/data/PlatformRepo.cs
@@ -27,7 +27,6 @@ namespace PlatformService.Data
         {
             return _context.Platforms.ToList();
         }
-
         public Platform GetPlatformById(int id)
         {
             return _context.Platforms.FirstOrDefault(p => p.Id == id);
