6dcaff91227e14883ffcbd0f424a03eb360360cb
wallaceleonel <wallace.leonell@outlook.com>
2022-02-28 00:25:43 -0300

    required

--- E3/CommandsService/Models/Command.cs
+++ E3/CommandsService/Models/Command.cs
@@ -1,17 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using CommandService.models;
 
 namespace CommandService.Models
 {
     public class Command
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
-
+        [Required]
         public string Howto  { get; set; }
-
+        [Required]
         public string CommandLine  { get; set; }
-
+        [Required]
         public int PlatformId { get; set; }
-
         public platform platform {get ; set;}
     }
 }
--- E3/CommandsService/Models/Platform.cs
+++ E3/CommandsService/Models/Platform.cs
@@ -1,9 +1,19 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CommandService.Models;
+
 namespace CommandService.models
 {
     public class platform
     {
+        [Key]
+        [Required]
         public int Id {get; set;}
+        [Required]
         public int ExternalID { get; set; }
+        [Required]
         public string  Name { get; set;}
+
+        public ICollection<Command> Commands { get; set;} = new List<Command>();
     }
 }
