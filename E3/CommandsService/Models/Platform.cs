132ae09975c74ac8acbc75dad56e4e720bcdceeb
wallaceleonel <wallace.leonell@outlook.com>
2022-02-28 00:09:28 -0300

    models Platform

---
+++ E3/CommandsService/Models/Platform.cs
@@ -0,0 +1,9 @@
+namespace CommandService.models
+{
+    public class platform
+    {
+        public int Id {get; set;}
+        public int ExternalID { get; set; }
+        public string  Name { get; set; }
+    }
+}
