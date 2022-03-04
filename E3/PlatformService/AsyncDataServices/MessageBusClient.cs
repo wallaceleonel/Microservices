71e3cca0d497b4096d8d892f3029efd742e4ee5e
wallaceleonel <wallace.leonell@outlook.com>
2022-03-04 14:34:43 -0300

    serializer

--- E3/PlatformService/AsyncDataServices/MessageBusClient.cs
+++ E3/PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 using PlatformService.Dtos;
 using RabbitMQ.Client;
@@ -7,16 +9,47 @@ namespace PlatformService.AsynDataServices
     public class MessageBusClient : IMessageBusClient
     {
         private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
 
         public MessageBusClient(IConfiguration configuration)
         {
              _configuration = configuration;
              var factory = new ConnectionFactory(){HostName = _configuration ["RabbitMQHost"], 
-             Port =int.Parse(_configuration["RabbitMQPort"])};
+                Port =int.Parse(_configuration["RabbitMQPort"])};
+            try
+            {
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+
+                _channel.ExchangeDeclare(exchange: "trigger", type : ExchangeType.Fanout);
+
+                _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+
+                Console.Write(" --> Connectede to MessageBus");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($" --> Could not connect to the Message Bus :{ex.Message}");
+            }
         }
-        public void PublishNewPlatform(PlatformPublishedDto platformNewPublishedDto)
+   public void PublishNewPlatform(PlatformPublishedDto platformPublishedDto)
         {
-            throw new System.NotImplementedException();
+            var message = JsonSerializer.Serialize(platformPublishedDto);
+
+            if ( _connection.IsOpen)
+            {
+                Console.WriteLine(" --> RabbitMQ Connecion open , senfing message .... ");
+            }
+            else
+            {
+                 Console.WriteLine(" --> RabbitMQ Connecion closed, not message .... ");
+            }
+        }
+        
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e )
+        {
+            Console.WriteLine(" -- > RabbitMQ Connection Shutdown");
         }
     }
 }
